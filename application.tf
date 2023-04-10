490fb8c3d0260881b7d89f5d9e2bdcf6ceed6bf6
algo7 <11154774+algo7@users.noreply.github.com>
2023-04-10 02:55:55 +0200

    add app access policy

--- application.tf
+++ application.tf
@@ -5,4 +5,18 @@ resource "cloudflare_access_application" "cf_application" {
   type                      = "self_hosted"
   session_duration          = "24h"
   auto_redirect_to_identity = false
+  app_launcher_visible      = false
+}
+
+
+resource "cloudflare_access_policy" "cf_application_policy" {
+  application_id = cloudflare_access_application.cf_application.id
+  account_id     = var.cloudflare_account_id
+  name           = "ServiceAuth"
+  precedence     = "1"
+  decision       = "allow"
+
+  include {
+    service_token = [cloudflare_access_service_token.cf_service_token.id]
+  }
 }
