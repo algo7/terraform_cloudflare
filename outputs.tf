5fff419f4946abe8d49ddfb9b4e33136b21dbce7
algo7 <11154774+algo7@users.noreply.github.com>
2023-04-10 03:49:34 +0200

    print tunnel secret, token, and service token to output

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,11 @@
 output "tunnel_secret" {
   value       = random_id.tunnel_secret.hex
   description = "Cloudflare Tunnel Secret"
-  sensitive   = true
 }
 
 output "tunnel_token" {
-  value       = cloudflare_tunnel.tunnel_1.tunnel_token
+  value       = nonsensitive(cloudflare_tunnel.tunnel_1.tunnel_token)
   description = "Cloudflare Tunnel Token"
-  sensitive   = true
 }
 
 output "tunnel_cname" {
@@ -15,9 +13,7 @@ output "tunnel_cname" {
   description = "CNAME record for accessing the tunnel"
 }
 
-
 output "service_token" {
-  value       = cloudflare_access_service_token.app_service_token.client_secret
+  value       = nonsensitive(cloudflare_access_service_token.app_service_token.client_secret)
   description = "Cloudflare Access Application Service Token"
-  sensitive   = true
 }
