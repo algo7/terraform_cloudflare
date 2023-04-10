66436cc9c7a814738b8feb0ea9cf089794854cc2
algo7 <11154774+algo7@users.noreply.github.com>
2023-04-10 03:35:56 +0200

    add cname record

--- records.tf
+++ records.tf
@@ -1,1 +1,8 @@
-
+# Add the tunnel CNAME record to the domain
+resource "cloudflare_record" "cf_tunnel_cname" {
+  zone_id = var.cloudflare_zone_id
+  name    = "test"
+  value   = cloudflare_tunnel.tunnel_1.cname
+  type    = "CNAME"
+  comment = "Cloudflare Tunnel"
+}
